6052b637e0926fc517d3a47fe8dae9db8089e87a
MrScriptX <scriptx001@gmail.com>
2017-11-13 10:44:21 +0100

    Arduino code

---
+++ Projet.ino
@@ -0,0 +1,39 @@
+int inputPin = 0;
+int outputPin[10] = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+void setup() {
+    Serial.begin(9600);
+    for(int i = 0; i < 10; i++)
+    {
+      pinMode(outputPin[i], OUTPUT);
+    }
+}
+
+void loop() {
+  //Serial.println(analogRead(inputPin)); 
+            
+  for(int i = 0; i < 10; i++)
+  {
+    digitalWrite(outputPin[i], HIGH);
+    
+    while(is_heart_beat)
+    {
+      delay(10);
+    }
+
+    digitalWrite(outputPin[i], LOW);
+  }
+}
+
+bool is_heart_beat()
+{
+  if(analogRead(inputPin) > 300)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
