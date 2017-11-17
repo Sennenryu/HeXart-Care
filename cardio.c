687022f93dba085ba8e106189fea2c838a4ec317
MrScriptX <scriptx001@gmail.com>
2017-11-17 10:23:49 +0100

    Add files via upload
    cardio.c and .h linked to .ino

---
+++ cardio.c
@@ -0,0 +1,66 @@
+#include "cardio.h"
+
+
+bool is_heart_beat(int *pin, int *nbBeat, int *beat_value)
+{ 
+  if(*beat_value > 650)
+  {
+    (*nbBeat)++;
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+int pulse_processing(unsigned int *last_time, int *nbBeat)
+{
+  int pulse = -1;
+  
+  if(time_elapsed(last_time) >= 15)
+  {
+    pulse  = get_beat(nbBeat)/15;
+    pulse  = pulse * 60;
+
+    set_time(&last_time);
+
+    return pulse;
+  }
+  else
+  {
+    return pulse;
+  }
+}
+
+unsigned int time_elapsed(unsigned int last_time)
+{
+  unsigned int current_time = 0;
+  unsigned int elapsed_time = 0;
+
+  current_time = get_time();
+  elapsed_time = current_time - last_time;
+
+  return elapsed_time;
+}
+
+unsigned int get_time()
+{
+  return (millis()/1000);
+}
+
+int get_beat(int *nbBeat)
+{
+  int temp = 0;
+  
+  temp = *nbBeat;
+  *nbBeat = 0;
+
+  return temp;
+}
+
+void set_time(unsigned int *timeVar)
+{
+  *timeVar = get_time();
+}
+
---
+++ cardio.h
@@ -0,0 +1,25 @@
+#ifndef CARDIO_H_INCLUDED
+#define CARDIO_H_INCLUDED
+
+#include <Arduino.h>
+#include <param.h>
+
+#define NO_VALUE -1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool is_heart_beat(int* pin, int *nbBeat, int *beat_value);
+int pulse_processing(unsigned int *last_time, int *nbBeat);
+unsigned int time_elapsed(unsigned int last_time);
+unsigned int get_time();
+int get_beat(int *nbBeat);
+void set_time(unsigned int *timeVar);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
